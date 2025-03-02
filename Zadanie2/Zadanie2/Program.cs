92a647861504579502f0b640473ad50d52b2dd8b
s20209 <s20209@pjwstk.edu.pl>
2025-03-02 09:37:32 +0100

    Modyfikacja 3

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -2,4 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
-//Console.WriteLine("Modyfikacja 3");
+Console.WriteLine("Modyfikacja 3");
