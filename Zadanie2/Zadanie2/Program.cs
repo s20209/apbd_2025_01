92246de0fb85cfc263a81bbee5a777339c71112e
s20209 <s20209@pjwstk.edu.pl>
2025-03-10 02:01:16 +0100

    commit

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -3,3 +3,28 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+double getAvg(int[] integers)
+{
+    int sum = 0;
+    double avg = 0;
+   for (int i = 0; i <= integers.Length; ++i)
+    {
+        sum += integers[i];
+        avg = sum / (i + 1);
+    }
+    return avg;
+}
+
+int getMax(int[] integers)
+{
+    int max;
+    for (int i = 0; i < integers.Length; i++)
+    {
+        if (max < integers[i])
+        {
+            max = integers[i];
+        }
+    }
+    return max;
+}
